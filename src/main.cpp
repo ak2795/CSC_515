c0d036f67d71d698b17e9dfbf7f829a2b1fff5dc
ak2795 <andrew.kym7@gmail.com>
2017-11-22 22:47:30 -0800

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,15 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        cout << "USAGE: no image specified." << endl;
+        return -1;
+    }
+
     Mat image;
     image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
 
     return 0;
 }
