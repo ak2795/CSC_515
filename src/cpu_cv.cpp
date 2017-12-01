e8f25b0f4732e625f71ff4c69356dfdc7f5ac293
ak2795 <andrew.kym7@gmail.com>
2017-11-30 18:59:59 -0800

    added training set code

--- src/cpu_cv.cpp
+++ src/cpu_cv.cpp
@@ -4,9 +4,23 @@
 #include <iostream>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <stdint.h>
 
 using namespace std;
 using namespace cv;
+
+// Structure for a Point
+struct Point {
+    int cluster_id;
+    unsigned char r, g, b;
+    int distance;
+};
+
+// Structure for a Pixel
+struct Pixel {
+    int cluster_id;
+    unsigned char r, g, b;
+};
 
 /*
  * Apply the mask to the image and output the result
@@ -22,14 +36,6 @@ Mat maskImg(String imPath, String mskPath) {
     
     // Bitwise AND the mask and image
     bitwise_and(image, image, result, mask=mask);
-   
-    // Testing for correct image reads
-    /*
-    imshow("Mask", mask);
-    imshow("Image", image);
-    imshow("Result", result);
-    waitKey(0);
-    */
 
     return result;
 }
@@ -52,22 +58,89 @@ Mat maskImgInverted(String imPath, String mskPath) {
     // Bitwise AND the mask and image
     bitwise_and(image, image, result, mask=invMask);
     
-    // Testing for correct image reads
-    
-    imshow("Mask", invMask);
-    imshow("Image", image);
-    imshow("Result", result);
-    waitKey(0);
-    
-    
     return result;
 }
 
-int main(int argc, char** argv) {
-    Mat test;
+/*
+ * Create the training set using the strawberry and non-strawberry pixels
+ * */
+vector<Pixel> createTrainingSet() {
+    vector<Pixel> trainingSet;
+    Mat strawberry, nonStrawberry;
+    String imFile, mskFile;
 
-    test = maskImg("PartA/s1.jpg", "PartA_Masks/s1.jpg");
-    test = maskImgInverted("PartA/s1.jpg", "PartA_Masks/s1.jpg");
+    // For every image in the training folder mask images for strawberry and non-strawberry pixels
+    // and add them to the training set vector
+    for (int i = 0; i < 1; i++) {
+        imFile = "PartA/s" + to_string(i + 10) + ".jpg";
+        mskFile = "PartA_Masks/s" + to_string(i + 10) + ".jpg";
+        strawberry = maskImg(imFile, mskFile);
+        nonStrawberry = maskImgInverted(imFile, mskFile);
+        
+
+        // ********** STRAWBERRY PIXELS ***********
+        // split the r, g, and b channels and create new pixel object
+        int channels = strawberry.channels();
+        int sRows = strawberry.rows;
+        int sCols = strawberry.cols * channels;
+
+        
+        // Check if continuous
+        if (strawberry.isContinuous()) {
+            sCols *= sRows;
+            sRows = 1;
+        }
+       
+        unsigned char r = 0, g = 0, b = 0; // to keep track of the rgb values
+        int count = 0; // keep track of columns (b = 0; g = 1; r = 2)
+        unsigned char* p = strawberry.ptr<unsigned char>();
+
+        for (int j = 0; j < sRows; j++) {
+            for (int k = 0; k < sCols; k++, p++) {
+                // save the values of b, g, and r regardless of value
+                switch(count) {
+                    case 0:
+                        b = *p;
+                        break;
+                    case 1:
+                        g = *p;
+                        break;
+                    case 2:
+                        r = *p;
+                        break;
+                    default:
+                        break;
+                }
+               
+                if (count == 2) {
+                    if (r || g || b) {
+
+                        // Create a Pixel object
+                        Pixel pixel = {1, r, g, b};
+                        
+                        // Add new pixel to the vector
+                        trainingSet.push_back(pixel);
+                    }
+                    b = g = r = 0;
+                    count = 0;
+                }
+
+                else
+                    count++;
+            }
+        }
+    }
+    cout << trainingSet.size() << endl;
+    return trainingSet;
+}
+
+
+int main(int argc, char** argv) {
+    Mat strawberry, nonStrawberry;
+    // Get the images for the strawberry and non-strawberry
+    strawberry = maskImg("PartA/s1.jpg", "PartA_Masks/s1.jpg");
+    nonStrawberry = maskImgInverted("PartA/s1.jpg", "PartA_Masks/s1.jpg");
+    createTrainingSet();
 
     return 0; 
 }
